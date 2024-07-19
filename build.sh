f98f655bb9c2dd48b9d911e50896db806dbcd859
LepkoQQ <lepko.san@gmail.com>
2024-07-19 12:11:53 +0200

    prepare for k8s

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
+docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
 
-sudo docker build -f ./front/Dockerfile -t brezgreha:latest ./front
-sudo docker tag brezgreha:latest rg.fr-par.scw.cloud/djnd/brezgreha:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/brezgreha:latest
+docker build -f ./front/Dockerfile -t brezgreha:latest ./front
+docker tag brezgreha:latest rg.fr-par.scw.cloud/djnd/brezgreha:latest
+docker push rg.fr-par.scw.cloud/djnd/brezgreha:latest
 
-sudo docker build -f ./counter/Dockerfile -t counter:latest ./counter
-sudo docker tag counter:latest rg.fr-par.scw.cloud/djnd/counter:latest
-sudo docker push rg.fr-par.scw.cloud/djnd/counter:latest
+docker build -f ./counter/Dockerfile -t counter:latest ./counter
+docker tag counter:latest rg.fr-par.scw.cloud/djnd/counter:latest
+docker push rg.fr-par.scw.cloud/djnd/counter:latest
